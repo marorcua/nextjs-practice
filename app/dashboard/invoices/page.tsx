a893bca90c150f931902e85cd40c3f6364da74b0
mario167 <mario@upya.io>
2025-02-03 12:02:18 +0100

    first push

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from 'react'
+
+export default function Page() {
+  return <div>Invoices</div>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function Page() {
+  return <div>Page</div>
+}
