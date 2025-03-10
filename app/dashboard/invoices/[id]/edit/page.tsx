39508bba806d82f1c43318b0988426c2e5fa3034
mario167 <mario@upya.io>
2025-03-10 18:27:26 +0100

    invoices crud

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+/** @format */
+
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
+import EditInvoiceForm from '@/app/ui/invoices/edit-form'
+import React from 'react'
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params
+  const id = params.id
+  const [invoice, customers] = await Promise.all([
+    await fetchInvoiceById(id),
+    await fetchCustomers()
+  ])
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true
+          }
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  )
+}
